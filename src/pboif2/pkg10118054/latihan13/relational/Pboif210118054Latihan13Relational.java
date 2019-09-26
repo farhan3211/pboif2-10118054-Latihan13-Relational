1023b615f67fd06a6c9cd83aad0f9c2c35f4d51e
farhan3211 <55830838+farhan3211@users.noreply.github.com>
2019-09-26 16:25:55 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118054/latihan13/relational/Pboif210118054Latihan13Relational.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118054.latihan13.relational;
+
+/**
+ *
+ * @author ASUS
+ * nama  :Farhan Milardi
+ * kelas :IF-2
+ * nim   :10118054
+ */
+public class Pboif210118054Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     * int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
